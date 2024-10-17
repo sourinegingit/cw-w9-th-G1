ff57169eea43dd1656afeeccd3dcffc596f85020
negin <neginsouri7979@gmail.com>
2024-10-17 16:36:49 +0430

    max and min number with speread

---
+++ q3/q3.js
@@ -0,0 +1,11 @@
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    alert("راست کلیلک کردن غیر فعاله");
+})
+
+// document.addEventListener("mouseup", function (event) {
+//     if (event.button === 2) {
+//         event.preventDefault();
+//         alert("راست کلیل کردن غیر فعاله ");
+//     }
+// })
